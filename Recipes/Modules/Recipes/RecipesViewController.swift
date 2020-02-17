fa6158272bdd6abc52cf22a08b14893e98e6e3e5
admin <vmormont@outlook.com>
2020-02-17 19:10:30 +0300

    ✨ Add Custom Segmented Control to Recipe module

--- Recipes/Modules/Recipes/RecipesViewController.swift
+++ Recipes/Modules/Recipes/RecipesViewController.swift
@@ -17,6 +17,42 @@ class RecipesViewController: UIViewController {
         view.backgroundColor = .white
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Recipes"
+        addControl()
+    }
+    
+    
+    func addControl() {
+        let titles = ["Breakfast", "Lunch", "Dinner", "Popular"]
+        let segmentedControl = CustomSegmentedControl(frame: CGRect(x: 0, y: 150, width: view.bounds.width, height: 25))
+        segmentedControl.delegate = self
+        segmentedControl.setSegmentItems(titles)
+        
+        segmentedControl.sliderBackgroundColor = .systemFill
+        segmentedControl.segmentsBackgroundColor = .systemTeal
+        
+        view.addSubview(segmentedControl)
     }
 
+}
+
+extension RecipesViewController: CustomSegmentedControlDelegate {
+    func didSelect(_ segmentIndex: Int) {
+        print("Selected index: \(segmentIndex)")
+    }
+    
+    
+}
+
+extension RecipesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+    
+    
 }
